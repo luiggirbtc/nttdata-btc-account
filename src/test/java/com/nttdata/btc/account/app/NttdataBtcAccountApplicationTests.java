427cc2bd2cbd3bd9fc91949950dbc4585cca009d
brayanlrs28 <riverasantiagobrayan@gmail.com>
2023-03-24 20:47:15 -0500

    update remove innecesary dependency

--- src/test/java/com/nttdata/btc/account/app/NttdataBtcAccountApplicationTests.java
+++ src/test/java/com/nttdata/btc/account/app/NttdataBtcAccountApplicationTests.java
@@ -3,7 +3,7 @@ package com.nttdata.btc.account.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class NttdataBtcAccountApplicationTests {
