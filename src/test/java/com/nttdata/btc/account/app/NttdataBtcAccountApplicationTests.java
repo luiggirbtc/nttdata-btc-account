c16677a9e78d6c68cc28bb8a20e04701b115c1d5
brayanlrs28 <riverasantiagobrayan@gmail.com>
2023-03-24 19:53:25 -0500

    update codesmells sonarqb

--- src/test/java/com/nttdata/btc/account/app/NttdataBtcAccountApplicationTests.java
+++ src/test/java/com/nttdata/btc/account/app/NttdataBtcAccountApplicationTests.java
@@ -3,11 +3,16 @@ package com.nttdata.btc.account.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 class NttdataBtcAccountApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
+		String expected = "btc-account";
+		String actual = "btc-account";
 
+		assertEquals(expected, actual);
+	}
 }
